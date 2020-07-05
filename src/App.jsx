f601c916fada0762882d1f5508c9c41d80c7dc68
Trent Costa <trentcosta@gmail.com>
2020-07-05 14:07:46 -0400

    deployed

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
     date: undefined,
   })
 
-  const [text, setText] = useState('')
+  const [amountToConvert, setAmountToConvert] = useState('')
   const [baseCurrency, setBaseCurrency] = useState('USD')
   const [currencyToConvert, setCurrencyToConvert] = useState('USD')
 
@@ -50,7 +50,7 @@ function App() {
     if (value === null) {
       return
     } else {
-      setText(value)
+      setAmountToConvert(value)
     }
   }
 
@@ -105,15 +105,16 @@ function App() {
       />
       <button
         type="button"
-        className="btn btn-primary ml-5 mb-2"
+        className="btn btn-primary ml-5 mb-2 mt-2"
         onClick={handleClickForConvert}
       >
-        Convert
+        Convert base currency
       </button>
 
       <ul className="list-group ml-5 mr-5">
         <input
-          type="text"
+          type="amountToConvert
+    "
           placeholder="Base Currency"
           value={baseCurrency}
           className="form-control mb-2"
@@ -121,143 +122,144 @@ function App() {
         />
 
         <input
-          type="text"
-          value={text}
+          type="amountToConvert
+    "
+          value={amountToConvert}
           onChange={textCurrencyInputToConvert}
           placeholder="Currency Amount to covert"
           className="form-control mb-2"
         />
         <li className="list-group-item">
           GBP rate: {currency.rates.GBP}
-          <div>Conversion Amount: {currency.rates.GBP * text}</div>
+          <div>Conversion Amount: {currency.rates.GBP * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           HKD rate: {currency.rates.HKD}
-          <div>Conversion Amount: {currency.rates.HKD * text}</div>
+          <div>Conversion Amount: {currency.rates.HKD * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           IDR: {currency.rates.ILS}
-          <div>Conversion Amount: {currency.rates.IDR * text}</div>
+          <div>Conversion Amount: {currency.rates.IDR * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           ILS: {currency.rates.ILS}
-          <div>Conversion Amount: {currency.rates.ILS * text}</div>
+          <div>Conversion Amount: {currency.rates.ILS * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           DKK: {currency.rates.DKK}
-          <div>Conversion Amount: {currency.rates.DKK * text}</div>
+          <div>Conversion Amount: {currency.rates.DKK * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           INR: {currency.rates.INR}
-          <div>Conversion Amount: {currency.rates.INR * text}</div>
+          <div>Conversion Amount: {currency.rates.INR * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           CHF: {currency.rates.CHF}
-          <div>Conversion Amount: {currency.rates.CHF * text}</div>
+          <div>Conversion Amount: {currency.rates.CHF * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           MXN: {currency.rates.MXN}
-          <div>Conversion Amount: {currency.rates.MXN * text}</div>
+          <div>Conversion Amount: {currency.rates.MXN * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           CZK: {currency.rates.CZK}
-          <div>Conversion Amount: {currency.rates.CZK * text}</div>
+          <div>Conversion Amount: {currency.rates.CZK * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           SGD: {currency.rates.SGD}
-          <div>Conversion Amount: {currency.rates.SGD * text}</div>
+          <div>Conversion Amount: {currency.rates.SGD * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           THB: {currency.rates.THB}
-          <div>Conversion Amount: {currency.rates.THB * text}</div>
+          <div>Conversion Amount: {currency.rates.THB * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           HRK: {currency.rates.HRK}
-          <div>Conversion Amount: {currency.rates.HRK * text}</div>
+          <div>Conversion Amount: {currency.rates.HRK * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           EUR: {currency.rates.EUR}
-          <div>Conversion Amount: {currency.rates.EUR * text}</div>
+          <div>Conversion Amount: {currency.rates.EUR * amountToConvert}</div>
         </li>
         <li className="list-group-item">
-          MYR: {currency.rates.MYR * text}
-          <div>Conversion Amount: {currency.rates.MYR * text}</div>
+          MYR: {currency.rates.MYR * amountToConvert}
+          <div>Conversion Amount: {currency.rates.MYR * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           NOK: {currency.rates.NOK}
-          <div>Conversion Amount: {currency.rates.NOK * text}</div>
+          <div>Conversion Amount: {currency.rates.NOK * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           CNY: {currency.rates.CNY}
-          <div>Conversion Amount: {currency.rates.CNY * text}</div>
+          <div>Conversion Amount: {currency.rates.CNY * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           BGN: {currency.rates.BGN}
-          <div>Conversion Amount: {currency.rates.BGN * text}</div>
+          <div>Conversion Amount: {currency.rates.BGN * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           PHP: {currency.rates.PHP}
-          <div>Conversion Amount: {currency.rates.PHP * text}</div>
+          <div>Conversion Amount: {currency.rates.PHP * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           PLN: {currency.rates.PLN}
-          <div>Conversion Amount: {currency.rates.PLN * text}</div>
+          <div>Conversion Amount: {currency.rates.PLN * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           ZAR: {currency.rates.ZAR}
-          <div>Conversion Amount: {currency.rates.ZAR * text}</div>
+          <div>Conversion Amount: {currency.rates.ZAR * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           CAD: {currency.rates.CAD}
-          <div>Conversion Amount: {currency.rates.CAD * text}</div>
+          <div>Conversion Amount: {currency.rates.CAD * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           ISK: {currency.rates.ISK}
-          <div>Conversion Amount: {currency.rates.ISK * text}</div>
+          <div>Conversion Amount: {currency.rates.ISK * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           BRL: {currency.rates.BRL}
-          <div>Conversion Amount: {currency.rates.BRL * text}</div>
+          <div>Conversion Amount: {currency.rates.BRL * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           RON: {currency.rates.RON}
-          <div>Conversion Amount: {currency.rates.RON * text}</div>
+          <div>Conversion Amount: {currency.rates.RON * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           NZD: {currency.rates.NZD}
-          <div>Conversion Amount: {currency.rates.NZD * text}</div>
+          <div>Conversion Amount: {currency.rates.NZD * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           TRY: {currency.rates.TRY}
-          <div>Conversion Amount: {currency.rates.TRY * text}</div>
+          <div>Conversion Amount: {currency.rates.TRY * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           JPY: {currency.rates.JPY}
-          <div>Conversion Amount: {currency.rates.JPY * text}</div>
+          <div>Conversion Amount: {currency.rates.JPY * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           RUB: {currency.rates.RUB}
-          <div>Conversion Amount: {currency.rates.RUB * text}</div>
+          <div>Conversion Amount: {currency.rates.RUB * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           KRW: {currency.rates.KRW}
-          <div>Conversion Amount: {currency.rates.KRW * text}</div>
+          <div>Conversion Amount: {currency.rates.KRW * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           USD: {currency.rates.USD}
-          <div>Conversion Amount: {currency.rates.USD * text}</div>
+          <div>Conversion Amount: {currency.rates.USD * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           AUD: {currency.rates.AUD}
-          <div>Conversion Amount: {currency.rates.AUD * text}</div>
+          <div>Conversion Amount: {currency.rates.AUD * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           HUF: {currency.rates.HUF}
-          <div>Conversion Amount: {currency.rates.HUF * text}</div>
+          <div>Conversion Amount: {currency.rates.HUF * amountToConvert}</div>
         </li>
         <li className="list-group-item">
           SEK: {currency.rates.SEK}
-          <div>Conversion Amount: {currency.rates.SEK * text}</div>
+          <div>Conversion Amount: {currency.rates.SEK * amountToConvert}</div>
         </li>
       </ul>
     </body>
